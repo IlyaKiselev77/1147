a99753b32378c5acc8f116714e86896237eef909
IlyaKiselev77 <90193620+IlyaKiselev77@users.noreply.github.com>
2021-09-07 09:50:33 +0300

    Update and rename README.md to task_1.1/Main.java

---
+++ task_1.1/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "имя: ";
+        System.out.println("Ilya");
+        int age = 29;
+        System.out.println("29");
+        String city = ("Город: ");
+        System.out.println("Москва");
+
+    }
+}
