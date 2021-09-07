78b9e4b12b6a2b1ab0492d82139161e1e8128778
IlyaKiselev77 <90193620+IlyaKiselev77@users.noreply.github.com>
2021-09-07 11:21:21 +0300

    Update Main.java

--- task_1.1/Main.java
+++ task_1.1/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        String name = "имя: ";
-        System.out.println("Ilya");
+        String name = "Имя: ";
+        System.out.println(name+("Илья"));
         int age = 29;
-        System.out.println("29");
+        String a = "Возраст: ";
+        System.out.println(a+(age));
         String city = ("Город: ");
-        System.out.println("Москва");
+        System.out.println(city+("Москва"));
 
     }
 }
