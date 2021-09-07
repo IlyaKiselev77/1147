80e7c1c3e11a08433317b6c759e90777438b4fa7
IlyaKiselev77 <90193620+IlyaKiselev77@users.noreply.github.com>
2021-09-07 11:23:09 +0300

    Create Main.java

---
+++ task_1.3/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 55;
+        int b = 81;
+        String s = "People: ";
+        System.out.println(s+(a+b));
+    }
+}
