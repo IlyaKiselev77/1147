96924671936cb114a20375ab28bea94732505ce9
IlyaKiselev77 <90193620+IlyaKiselev77@users.noreply.github.com>
2021-09-07 11:28:08 +0300

    Create Main.java

---
+++ task_1.4/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 25;
+        String core = "Корень равен: ";
+        System.out.println(core+(number*number));
+    }
+}
