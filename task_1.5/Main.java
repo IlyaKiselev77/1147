fc7e94109504858c06d6aa0efbc7afb9f824abdf
IlyaKiselev77 <90193620+IlyaKiselev77@users.noreply.github.com>
2021-09-07 11:30:57 +0300

    Create Main.java

---
+++ task_1.5/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        float a = scan.nextFloat();
+        float b = (9f / 5f) * a + 32f;
+        System.out.println(b);
+
+    }
+}
