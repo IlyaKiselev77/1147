128f97a58b17023d7079a4f233804b23478fb6b4
IlyaKiselev77 <90193620+IlyaKiselev77@users.noreply.github.com>
2021-09-09 20:38:03 +0300

    Create Main.java

---
+++ task_2.5/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        System.out.println(a);
+        if(a == 10)
+        {System.out.println("Верно");}
+        else
+        {System.out.println("Неверно");}
+      
+    }
+}
