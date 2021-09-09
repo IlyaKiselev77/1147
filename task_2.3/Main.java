9950ce662da18abc477f7072fe30abdd20432610
IlyaKiselev77 <90193620+IlyaKiselev77@users.noreply.github.com>
2021-09-09 15:11:55 +0300

    Create Main.java

---
+++ task_2.3/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i=10; i >0; i--) {
+            for (int a=i; a < 11;a++) {
+                System.out.print(8);}
+            System.out.println("");
+        }
+    }
+}
