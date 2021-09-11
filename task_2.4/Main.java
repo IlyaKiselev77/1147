f5add5cf6d0b0fbd5fdd26159f4468dd4894dc70
IlyaKiselev77 <90193620+IlyaKiselev77@users.noreply.github.com>
2021-09-11 17:57:35 +0300

    Create Main.java

---
+++ task_2.4/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Вклад: ");
+        float a =scanner.nextInt();
+        System.out.println("Под процент: ");
+        float b = scanner.nextInt();
+        for (int i = 0; i < 6; i = i+1) {
+            a = a + (a*b/100);
+            System.out.println(a);
+            }
+        }
+    }
