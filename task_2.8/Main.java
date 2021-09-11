64366136b88acf8538bbf0406a6635cb818cc1ff
IlyaKiselev77 <90193620+IlyaKiselev77@users.noreply.github.com>
2021-09-11 21:34:21 +0300

    Create Main.java

---
+++ task_2.8/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] cats = {2,3,4,5};
+        int d=0;
+        for (int i = 0; i < cats.length; i++) {
+            d = d + cats[i];
+        }
+            System.out.println(d);
+    }
+}
