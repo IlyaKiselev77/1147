eda4f0d10b430db0aec77c1121855bee5e70ddfd
IlyaKiselev77 <90193620+IlyaKiselev77@users.noreply.github.com>
2021-09-12 11:53:14 +0300

    Create Main.java

---
+++ task_2.9/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean cats1 = false;
+        int[] cats = {32, 71, 9, 5, 12, 43, 57};
+        for (int i = 0; i < cats.length; i++) {
+            if (cats[i] == 5) {
+                cats1 = true;}
+        }
+        System.out.println(cats1);
+    }}
