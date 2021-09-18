d4b3c42aa5cf1a0d0e1b1a3e19301265614d6706
IlyaKiselev77 <90193620+IlyaKiselev77@users.noreply.github.com>
2021-09-18 14:22:43 +0300

    Create Main.java

---
+++ task_3.5/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int num[] = {4,2,6,2,65};
+        if(num.length>=1)  {
+            System.out.print(num[0]+","+num[1]+","+num[2]+","+num[3]+","+num[4]);
+        }
+    }}
