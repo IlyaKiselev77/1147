c53c1f9bc86ad11bac0a0cc11a32854f88149af6
IlyaKiselev77 <90193620+IlyaKiselev77@users.noreply.github.com>
2021-09-21 13:46:05 +0300

    Update Main.java

--- task_3.3/Main.java
+++ task_3.3/Main.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void function(int a, int b) {
+
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Первое число: ");
         int a =scanner.nextInt();
         System.out.println("Второе число: ");
         int b = scanner.nextInt();
-
+        function(a,b);
         if (a==b) {
             System.out.println("true");
         }else {System.out.println("false");}
